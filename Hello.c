4c0cf3ee8368010dcbf0475f29663ba080f9a4ff
samiksharma098 <sharmasamik098@gmail.com>
2024-12-02 08:57:30 +0545

    initialized normal html code

---
+++ Hello.c
@@ -0,0 +1,16 @@
+// area of square
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int side;
+
+    printf("Enter the side of square: \n");
+    scanf("%d",&side);
+
+
+    printf("area of square is : %d", side*side);
+
+
+    return 0;
+}
